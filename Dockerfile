672a1ec18b1ab58121aecb7309f429d9c2140a08
shouq01 <sho8.tt@gmail.com>
2022-11-13 12:30:18 +0300

    filled installPackages & createShortcuts | Shouq

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:alpine
-WORKDIR /app
-COPY . .
-RUN yarn install
-CMD ["node", "src/index.js"]
-EXPOSE 3000
+FROM node:alpine      
+WORKDIR /app          
+COPY . .              
+RUN yarn install      
+CMD ["node", "src/index.js"]    
+EXPOSE 3000      
--- scripts/13_setup_machine.sh
+++ scripts/13_setup_machine.sh
@@ -3,10 +3,30 @@
 
 function installPackages(){
 # TODO: Install needed libries
+# Install Brew
+echo " Brew Installation"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Git through Brew
+echo "git Installation"
+brew install git
+# Create the Alias in the Bash profile 
 }
 
 function createShortcuts(){
-# TODO: Create aliases and add them to your shell profile (~/.zshrc or ~/.bashrc)
+# TODO: Create 2 aliases and add them to your shell profile (~/.zshrc or ~/.bashrc)
+ 
+# List all hidden files
+echo ' alias list = "ls -a" ' >> ~/.bash_profile
+
+# Install the needed packages
+echo ' alias installPackages = "./13_setup_machine.sh && installPackages" ' >> ~/.bash_profile
+
+# Create Dummy hello world txt file inside HelloApp directory
+echo ' alias Hello="mkdir HelloApp &&  Hello World File ! > Hello.txt | mv Hello.txt HelloApp" ' >> ~/.bash_profile
+
+echo 'Re-Loading Shortcuts'
+source ~/.bash_profile
+
 }
 
 
